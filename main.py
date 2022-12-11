a45b5a17e2291e2218d558c45db2e44adc0e9f27
Devashish Chakraborty <devashishchakra@gmail.com>
2022-12-11 05:42:09 +0530

    Added Assets

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from ursina import *
 
+def update():
+    if held_keys['a']:
+        test_square.x -= 2 * time.dt
+
 app = Ursina()
 
+test_square = Entity(model="quad", color=color.red, scale=(1,4), position=(2,2))
 
+sans_texture = load_texture('assets/Sans.png')
+sans = Entity(model="quad", texture=sans_texture)
 
 app.run()
